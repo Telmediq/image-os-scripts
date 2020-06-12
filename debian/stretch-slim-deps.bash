441e552ffd66595ac17c75ae598a745aaab24f22
Chris Dobbyn <cdobbyn@gmail.com>
2020-06-11 21:51:28 -0700

    Separate into alpine/debian script files

---
+++ debian/stretch-slim-deps.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+function do_work() {
+  # Dependencies
+  install_deps
+
+  # Final Step
+  remove_self
+}
+
+function install_deps() {
+  # Dependencies
+  deps=()
+
+  # Generic utilities
+  deps+=(grep util-linux coreutils findutils)
+
+  # Web utilities
+  deps+=(wget curl ca-certificates)
+
+  # Process diagnostics
+  deps+=(procps htop sysstat strace)
+
+  # Network diagnostics
+  deps+=(nethogs inetutils dnsutils)
+
+  # Install Dependencies
+  apt-get update
+  apt-get install -y "${deps[@]}"
+  apt-get clean
+}
+
+function remove_self() {
+  SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+  rm -rf ${SCRIPT_PATH}/alpine.bash
+}
+
+set -ex
+do_work
---
+++ debian/stretch-slim-user.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "this script has not been created yet. Be the first person to create it!"
